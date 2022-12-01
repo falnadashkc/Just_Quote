0a4c6ea85661338b671722b7362b660304714524
falnadashkc <113893312+falnadashkc@users.noreply.github.com>
2022-12-01 13:12:18 +0700

    Just_Quote

---
+++ app/src/main/java/com/si5b/just_quote/Activity/MainActivity.java
@@ -0,0 +1,16 @@
+    package com.si5b.just_quote.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.si5b.just_quote.R;
+
+    public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
